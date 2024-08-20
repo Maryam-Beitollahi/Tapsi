6d8385d52d490a871dcb86e55ee93df79fd577c6
Maryam <maryambeitollahi71@gmail.com>
2024-08-20 13:04:04 +0330

    initial value

--- src/main.js
+++ src/main.js
@@ -4,8 +4,4 @@ toggler.addEventListener("click", (e) => {
     navbar.classList.toggle("nav__expanded");
     });
 
-const tabs = document.querySelectorAll (".tab");
-const tabContents = document.querySelectorAll (".tab-content");
-tabs.forEach ((tab) => {...
-});
 
